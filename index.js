548fab2e8110d3b69887502ccf3d13a8d8672814
Muthiira <calebmuchiri48@gmail.com>
2022-08-02 21:52:34 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(x, y) {
+	console.log(add);
+}
+
+function subtract(x, y) {
+	console.log(subtract);
+}
+
+function divide(x, y) {
+	console.log(divide);
+}
+
+function multiply(x, y) {
+	console.log(multiply);
+}
+
+function add(x, y) {
+	return (x + y);
+}
+
+function subtract(x, y) {
+	return (x - y);
+}
+function divide(x, y) {
+	return (x / y);
+}
+
+function multiply(x, y) {
+	return (x * y);
+}
+function increment(x) {
+	return (x + 1)
+}
+function decrement(x) {
+	return (x - 1)
+}
+
+function makeInt(n, base10) {
+	let a = parseInt(n, base10);
+	return a;
+}
+
+function preserveDecimal(n,base10){
+	let a = parseFloat(n,base10);
+	 return a;
+}
